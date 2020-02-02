66bccc839ff74ec5dab4a84b4423eb30d6e76ad4
张建 <475218106@qq.com>
2020-02-02 21:13:39 +0800

    feat：增加setting页面

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,6 +4,7 @@
  */
 
 const router = require('koa-router')()
+const { loginRedirect } = require('../../middlewares/loginChecks')
 
 function getLoginInfo (ctx) {
   let data = {
@@ -25,6 +26,10 @@ router.get('/login', async (ctx, next) => {
 
 router.get('/register', async (ctx, next) => {
   await ctx.render('register', getLoginInfo(ctx))
+})
+
+router.get('/setting', loginRedirect, async (ctx, next) => {
+  await ctx.render('setting', ctx.session.userInfo)
 })
 
 module.exports = router
