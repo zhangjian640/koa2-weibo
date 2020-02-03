88ca113a6ab1dc0727b1ca107809bc1f0d8dec2e
张建 <475218106@qq.com>
2020-02-03 16:21:01 +0800

    feat:修改用户信息单元测试

--- test/User/login.test.js
+++ test/User/login.test.js
@@ -17,36 +17,46 @@ let COOKIE = ''
 
 describe('注册', () => {
   test('用户注册，应该成功', async () => {
-    const res = await server.post('/api/user/register').send(testUser)
+    const res = await server
+      .post('/api/user/register')
+      .send(testUser)
     expect(res.body.errno).toBe(0)
   })
 
   test('重复注册, 应该失败', async () => {
-    const res = await server.post('/api/user/register').send(testUser)
+    const res = await server
+      .post('/api/user/register')
+      .send(testUser)
     expect(res.body.errno).not.toBe(0)
   })
 
   test('查询注册的用户名，应该存在', async () => {
-    const res = await server.post('/api/user/isExist').send({ userName })
+    const res = await server
+      .post('/api/user/isExist')
+      .send({ userName })
     expect(res.body.errno).toBe(0)
   })
 
   test('json schema 检测，非法的格式，注册应该失败', async () => {
-    const res = await server.post('/api/user/register').send({
-      userName: '123',
-      password: 'a',
-      gender: 'abc'
-    })
+    const res = await server
+      .post('/api/user/register')
+      .send({
+        userName: '123',
+        password: 'a',
+        gender: 'abc'
+      })
     expect(res.body.errno).not.toBe(0)
   })
 })
 
 describe('登录', () => {
   test('应该登录成功', async () => {
-    const res = await server.post('/api/user/login').send({
-      userName,
-      password
-    })
+    const res = await server
+      .post('/api/user/login')
+      .send({
+        userName,
+        password
+      })
     expect(res.body.errno).toBe(0)
 
     // 获取cookie
@@ -54,13 +64,47 @@ describe('登录', () => {
 
   })
 
+  test('修改基本信息，应该成功', async () => {
+    const res = await server
+      .patch('/api/user/changeInfo')
+      .send({
+        nickName: '测试昵称',
+        city: '测试城市',
+        picture: '/test.png'
+      })
+      .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+  })
+
+  test('修改密码应该成功', async () => {
+    const res = await server
+      .patch('/api/user/changePassword')
+      .send({
+        password,
+        newPassword: `p_${Date.now()}`
+      })
+      .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+  })
+
   test('删除用户应该成功', async () => {
-    const res = await server.post('/api/user/delete').set('cookie', COOKIE)
+    const res = await server
+      .post('/api/user/delete')
+      .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+  })
+
+  test('退出登录应该成功', async () => {
+    const res = await server
+      .post('/api/user/logout')
+      .set('cookie', COOKIE)
     expect(res.body.errno).toBe(0)
   })
 
   test('删除用户后，再次查询注册的用户名，应该不存在', async () => {
-    const res = await server.post('/api/user/isExist').send({ userName })
+    const res = await server
+      .post('/api/user/isExist')
+      .send({ userName })
     expect(res.body.errno).not.toBe(0)
   })
 })
